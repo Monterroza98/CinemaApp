73be31b15f0e70b04da6bc1abe4fdd2ac83a7203
Kevin Joel Rojas Monterroza <kejoromo1998@gmail.com>
2021-09-15 21:26:30 -0600

    update miercoles 15

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,app,request,jsonify,redirect,url_for
 from flask_cors import CORS,cross_origin
+import pandas as pd
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -9,10 +10,15 @@ cors = CORS(app)
 def upload_file(): 
     response=jsonify(message="exito") 
 
-    f = request.files.getlist('file')
+    #f = request.files.getlist('file')
+    f = request.files['file']
 
     print(f)
     #return(response,200)
+
+    df = pd.read_csv(f, encoding='latin-1', error_bad_lines=False)
+    print(df)
+
     return redirect("http://127.0.0.1")
 
 
