a8162d20dfe01afd84fa532ea6d38da3bb5cdb66
Kevin Joel Rojas Monterroza <kejoromo1998@gmail.com>
2021-09-16 22:28:43 -0600

    update jueves

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,app,request,jsonify,redirect,url_for
 from flask_cors import CORS,cross_origin
 import pandas as pd
+from werkzeug.datastructures import Headers
+import xlrd
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -13,11 +15,18 @@ def upload_file():
     #f = request.files.getlist('file')
     f = request.files['file']
 
-    print(f)
+    #print(j)
+
+    #print(f)
     #return(response,200)
 
-    df = pd.read_csv(f, encoding='latin-1', error_bad_lines=False)
-    print(df)
+    j= pd.read_excel(f.stream, skiprows=2, index_col=None, usecols=[1,2,3,4,5])
+
+    #df = j.to_csv(encoding='latin-1', header=True)
+
+    #j.drop(axis=1, columns=[5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46])
+    #j.drop(axis=1, inplace=True, columns=['Rank'])
+    print(j)
 
     return redirect("http://127.0.0.1")
 
